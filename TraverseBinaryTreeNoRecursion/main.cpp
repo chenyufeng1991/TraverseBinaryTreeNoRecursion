620559d2c98eed63389d45465daef0333bb6f079
陈宇峰 <yufengcode@gmail.com>
2016-10-03 00:36:15 +0800

    完成后序遍历的非递归实现。

--- TraverseBinaryTreeNoRecursion/main.cpp
+++ TraverseBinaryTreeNoRecursion/main.cpp
@@ -19,6 +19,7 @@ typedef struct TreeNode{
     int data;
     struct TreeNode *lChild;
     struct TreeNode *rChild;
+    bool isVisited; // 是否被访问过的标记
 }Node;
 
 void CreateBinaryTree(Node **node)
@@ -86,6 +87,32 @@ void MidOrderNoRecursion(Node *node, stack<Node *>&sta)
     }
 }
 
+// 后序遍历
+void PostOrderNoRecursion(Node *node, stack<Node *>&sta)
+{
+    while (!sta.empty())
+    {
+        while (node->lChild != NULL)
+        {
+            sta.push(node->lChild);
+            node = node->lChild;
+        }
+
+        Node *temp = sta.top();
+        if (temp->rChild == NULL || temp->rChild->isVisited == true)
+        {
+            cout << temp->data << " ";
+            temp->isVisited = true;
+            sta.pop();
+        }
+        else if(temp->rChild != NULL && temp->isVisited == false)
+        {
+            sta.push(temp->rChild);
+            node = temp->rChild;
+        }
+    }
+}
+
 int main(int argc, const char * argv[])
 {
     cout << "请输入二叉树的节点，输入-1结束:";
@@ -106,8 +133,12 @@ int main(int argc, const char * argv[])
     MidOrderNoRecursion(node, nodeStackM);
     cout << endl;
 
-
-
+    cout << "非递归后序遍历的结果为：";
+    stack<Node *> nodeStackP;
+    // 首先根节点入栈
+    nodeStackP.push(node);
+    PostOrderNoRecursion(node, nodeStackP);
+    cout << endl;
 
     return 0;
 }
