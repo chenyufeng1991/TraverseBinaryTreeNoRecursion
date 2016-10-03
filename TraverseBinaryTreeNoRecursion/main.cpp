c98cfdb5b55d57ea1a05ceab5ea3b5b44978a5ae
陈宇峰 <yufengcode@gmail.com>
2016-10-03 11:34:51 +0800

    comment

--- TraverseBinaryTreeNoRecursion/main.cpp
+++ TraverseBinaryTreeNoRecursion/main.cpp
@@ -44,6 +44,10 @@ void CreateBinaryTree(Node **node)
 }
 
 // 先序遍历
+/**
+ *  首先把根节点入栈，并且在根节点出栈的时候访问根节点；判断根节点的右子树是否为空，若不为空，则右孩子入栈；判断根节点的左孩子是否为空，若不为空，则左孩子入栈。    当栈不为空时，重复以上操作。
+ *
+ */
 void PreOrderNoRecursion(Node *node, stack<Node *> &sta)
 {
     while (!sta.empty())
@@ -66,6 +70,10 @@ void PreOrderNoRecursion(Node *node, stack<Node *> &sta)
 }
 
 // 中序遍历
+/**
+ *  首先根节点入栈，判断根节点的左孩子是否为空，若不为空，则入栈，并不断移动node指针指向左孩子，直到左孩子节点为空。
+    然后弹出栈顶元素并访问该元素。判断弹出元素的右孩子是否为空，若不为空，则把右孩子入栈，并把node指针指向右孩子。重复以上操作。
+ */
 void MidOrderNoRecursion(Node *node, stack<Node *>&sta)
 {
     while (!sta.empty())
@@ -88,6 +96,15 @@ void MidOrderNoRecursion(Node *node, stack<Node *>&sta)
 }
 
 // 后序遍历
+/**
+ *  在节点的结构体中加入一个参数isVisited,表示某个节点是否被访问过。
+ 
+ 首先根节点入栈，然后判断左孩子是否为空，若不为空，则左孩子入栈，并移动node到左孩子，直到左孩子为空。
+ 然后判断栈顶元素，如果栈顶元素没有右孩子或者右孩子已经被访问过，那么就访问该栈顶元素，并出栈；如果栈顶元素有右孩子并且右孩子没有被访问过，那么把右孩子入栈，把node指针指向右孩子。
+ 若栈不为空，则重复以上操作。
+
+ *
+ */
 void PostOrderNoRecursion(Node *node, stack<Node *>&sta)
 {
     while (!sta.empty())
